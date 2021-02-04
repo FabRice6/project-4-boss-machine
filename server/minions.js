a4e679cd1699496f7f3d7b5a3c4bd6782d960774
Fabrice Verhaert <fabriceverhaert@gmail.com>
2021-02-04 21:54:35 +0100

    work routes

--- server/minions.js
+++ server/minions.js
@@ -52,4 +52,48 @@ minionsRouter.delete('/:minionId', (req, res, next) => {
     res.send()
 })
 
+minionsRouter.get('/:minionId/work', (req, res, next) => {
+    const work = db.getAllFromDatabase('work').filter((singleWork) => {
+        return singleWork.minionId == req.params.minionId
+    })
+    res.send(work)
+})
+
+minionsRouter.post('/:minionId/work', (req, res, next) => {
+    res.status(201).send(db.addToDatabase('work', req.body))
+})
+
+minionsRouter.param('workId', (req, res, next, id) => {
+    const work = db.getFromDatabaseById('work', id)
+    if (work) {
+        req.work = work
+        next()
+    } else {
+        res.status(404).send()
+    }
+
+})
+
+minionsRouter.put('/:minionId/work/:workId', (req, res, next) => {
+    if (req.body.minionId !== req.params.minionId) {
+        res.status(400).send()
+    } else {
+        const newWork = db.updateInstanceInDatabase('work', req.body)
+        if (newWork) {
+            res.send(newWork)
+        } else {
+            res.status(404).send()
+        }
+    }
+})
+
+minionsRouter.delete('/:minionId/work/:workId', (req, res, next) => {
+    const deleted = db.deleteFromDatabasebyId('work', req.params.workId)
+    if (deleted) {
+        res.status(204).send()
+    } else {
+        res.status(500).send()
+    }
+})
+
 module.exports = minionsRouter;
